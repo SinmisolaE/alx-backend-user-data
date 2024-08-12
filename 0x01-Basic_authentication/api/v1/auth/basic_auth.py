5700f292ac4d317d280dc34d8f43b42fcfa97fd3
SinmisolaE <sinmisolaajao@gmail.com>
2024-08-12 05:42:54 +0000

    just a typo error

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,12 +14,6 @@ class BasicAuth(Auth):
                                             authorization_header: str) -> str:
         """ returns the Base64 part of the
             Authorization header for a Basic Authentication"""
-            def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
-        """
-        Extracts the Base64 part of the Authorization header for a Basic
-        Authorization
-        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
