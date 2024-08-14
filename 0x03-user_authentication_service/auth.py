1ecf75779eedc2427253b7bfceca8655d51e3d2f
SinmisolaE <sinmisolaajao@gmail.com>
2024-08-14 19:01:35 +0000

    first set done

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+  method that takes in a password string arguments and returns bytes
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """ eturned bytes is a salted hash of the input password
+      hashed with bcrypt.hashpw
+    """
+    passwd = password.encode('utf-8')
+    return bcrypt.hashpw(passwd, bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+          return a User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hash_password = _hash_password(password)
+            user = User(email=email, password=hash_password)
+            self._db.add_user(user)
+            return user
+            
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password) -> User:
+        """ returns a User object """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ takes in arbitrary keyword arguments
+        returns the first row found in the users"""
+        all_users = self._session.query(User)
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise InvalidRequestError
+            for user in all_users:
+                if getattr(user, k) == v:
+                    return user
+            raise NoResultFound
+
+    def update_user(self, id, **kwargs) -> None:
+        """ finds and updates a user"""
+        for k, v in kwargs.items():
+            if k not in User.__dict__:
+                raise ValueError
+            try:
+                user = self.find_user_by(id=id)
+                setattr(user, k, v)
+                return None
+            except InvalidRequestError or NoResultFound:
+                raise ValueError
