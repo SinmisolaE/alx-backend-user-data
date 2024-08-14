d60a283bab12aecef75bf3c54602e57f51923230
SinmisolaE <sinmisolaajao@gmail.com>
2024-08-14 22:14:05 +0000

    second set done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ a basic Flask app"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from auth import Auth
 
 
@@ -14,6 +14,7 @@ def basic():
     """  return a JSON payload of the form:"""
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def user():
     """ implement the end-point to register a user"""
@@ -24,6 +25,7 @@ def user():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
