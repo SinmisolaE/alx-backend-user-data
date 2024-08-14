ffacb7eb2b039304e68deec2d0ba08f1c02fe228
SinmisolaE <sinmisolaajao@gmail.com>
2024-08-14 21:54:57 +0000

    second set done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,9 +7,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-app.route('/', methods=['GET'])
-
-
+@app.route('/', methods=['GET'], strict_slashes=False)
 def basic():
     """  return a JSON payload of the form:"""
     return jsonify({"message": "Bienvenue"})
